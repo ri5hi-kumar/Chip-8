ecc76fba39c77fa318eb1d1ee29fd1ca5af546f5
Rishi Kumar <rishikumar10dec@gmail.com>
2024-06-14 15:52:21 +0530

    added opcodes in comments

--- src/instructions.c
+++ src/instructions.c
@@ -9,6 +9,7 @@
 */
 
 /*
+ * 00E0 - CLS
  * Clear the display.
  */
 void cls(Chip8 *chip8) {
@@ -21,6 +22,7 @@ void cls(Chip8 *chip8) {
 }
 
 /*
+ * 00EE - RET
  * Return from a subroutine.
  * The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.
  */
@@ -31,6 +33,7 @@ void ret(Chip8 *chip8) {
 }
 
 /*
+ * 1nnn - JP addr
  * Jump to location nnn.
  * The interpreter sets the program counter to nnn.
  */
@@ -40,6 +43,7 @@ void jmp(Chip8 *chip8) {
 }
 
 /*
+ * 2nnn - CALL addr
  * Call subroutine at nnn.
  * The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
  */
@@ -51,6 +55,7 @@ void call(Chip8 *chip8) {
 }
 
 /*
+ * 3xkk - SE Vx, byte
  * Skip next instruction if Vx = kk.
  * The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
  */
@@ -65,6 +70,7 @@ void se_Vx_kk(Chip8 *chip8) {
 }
 
 /*
+ * 4xkk - SNE Vx, byte
  * Skip next instruction if Vx != kk.
  * The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
  */
@@ -79,6 +85,7 @@ void sne_Vx_kk(Chip8 *chip8) {
 }
 
 /*
+ * 5xy0 - SE Vx, Vy
  * Skip next instruction if Vx = Vy.
  * The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
  */
@@ -93,6 +100,7 @@ void se_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 6xkk - LD Vx, byte
  * Set Vx = kk.
  * The interpreter puts the value kk into register Vx.
  */
@@ -104,6 +112,7 @@ void ld_Vx(Chip8 *chip8) {
 }
 
 /*
+ * 7xkk - ADD Vx, byte
  * Set Vx = Vx + kk.
  * Adds the value kk to the value of register Vx, then stores the result in Vx.
  */
@@ -115,6 +124,7 @@ void add_Vx_kk(Chip8 *chip8) {
 }
 
 /*
+ * 8xy0 - LD Vx, Vy
  * Set Vx = Vy.
  * Stores the value of register Vy in register Vx.
  */
@@ -126,6 +136,7 @@ void ld_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xy1 - OR Vx, Vy
  * Set Vx = Vx OR Vy.
  * Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx.
  * A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. 
@@ -139,6 +150,7 @@ void or_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xy2 - AND Vx, Vy
  * Set Vx = Vx AND Vy.
  * Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx.
  * A bitwise AND compares the corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1.
@@ -152,6 +164,7 @@ void and_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xy3 - XOR Vx, Vy
  * Set Vx = Vx XOR Vy.
  * Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx.
  * An exclusive OR compares the corrseponding bits from two values, and if the bits are not both the same,
@@ -165,6 +178,7 @@ void xor_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xy4 - ADD Vx, Vy
  * Set Vx = Vx + Vy, set VF = carry.
  * The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,)
  * VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
@@ -183,6 +197,7 @@ void add_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xy5 - SUB Vx, Vy
  * Set Vx = Vx - Vy, set VF = NOT borrow.
  * If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
  */
@@ -199,6 +214,7 @@ void sub_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xy6 - SHR Vx {, Vy}
  * Set Vx = Vx SHR 1.
  * If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
  */
@@ -210,6 +226,7 @@ void shr(Chip8 *chip8) {
 }
 
 /*
+ * 8xy7 - SUBN Vx, Vy
  * Set Vx = Vy - Vx, set VF = NOT borrow.
  * If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
  */
@@ -226,6 +243,7 @@ void subn_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * 8xyE - SHL Vx {, Vy}
  * Set Vx = Vx SHL 1.
  * If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
  */
@@ -237,6 +255,7 @@ void shl(Chip8 *chip8) {
 }
 
 /*
+ * 9xy0 - SNE Vx, Vy
  * Skip next instruction if Vx != Vy.
  * The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
  */
@@ -251,6 +270,7 @@ void sne_Vx_Vy(Chip8 *chip8) {
 }
 
 /*
+ * Annn - LD I, addr
  * Set I = nnn.
  * The value of register I is set to nnn.
  */
@@ -261,6 +281,7 @@ void ldi(Chip8 *chip8) {
 }
 
 /*
+ * Bnnn - JP V0, addr
  * Jump to location nnn + V0.
  * The program counter is set to nnn plus the value of V0.
  */
@@ -270,6 +291,7 @@ void jmp_V0(Chip8 *chip8) {
 }
 
 /*
+ * Cxkk - RND Vx, byte
  * Set Vx = random byte AND kk.
  * The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk.
  * The results are stored in Vx. See instruction 8xy2 for more information on AND.
@@ -284,6 +306,7 @@ void rnd(Chip8 *chip8) {
 }
 
 /*
+ * Dxyn - DRW Vx, Vy, nibble
  * Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
  * The interpreter reads n bytes from memory, starting at the address stored in I. These bytes are then displayed as
  * sprites on screen at coordinates (Vx, Vy). Sprites are XORed onto the existing screen. If this causes any pixels to
@@ -324,6 +347,7 @@ void drw(Chip8 *chip8) {
 }
 
 /*
+ * Ex9E - SKP Vx
  * Skip next instruction if key with the value of Vx is pressed.
  * Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
  */
@@ -337,6 +361,7 @@ void skp(Chip8 *chip8) {
 }
 
 /*
+ * ExA1 - SKNP Vx
  * Skip next instruction if key with the value of Vx is not pressed.
  * Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
  */
@@ -350,6 +375,7 @@ void sknp(Chip8 *chip8) {
 }
 
 /*
+ * Fx07 - LD Vx, DT
  * Set Vx = delay timer value.
  * The value of DT is placed into Vx.
  */
@@ -360,6 +386,7 @@ void ld_Vx_dt(Chip8 *chip8) {
 }
 
 /*
+ * Fx0A - LD Vx, K
  * Wait for a key press, store the value of the key in Vx.
  * All execution stops until a key is pressed, then the value of that key is stored in Vx.
  */
@@ -384,6 +411,7 @@ void ld_Vx_key(Chip8 *chip8) {
 }
 
 /*
+ * Fx15 - LD DT, Vx
  * Set delay timer = Vx.
  * DT is set equal to the value of Vx.
  */
@@ -394,6 +422,7 @@ void ld_dt_Vx(Chip8 *chip8) {
 }
 
 /*
+ * Fx18 - LD ST, Vx
  * Set sound timer = Vx.
  * ST is set equal to the value of Vx.
  */
@@ -404,6 +433,7 @@ void ld_st_Vx(Chip8 *chip8) {
 }
 
 /*
+ * Fx1E - ADD I, Vx
  * Set I = I + Vx.
  * The values of I and Vx are added, and the results are stored in I.
  */
@@ -414,6 +444,7 @@ void add_i_Vx(Chip8 *chip8) {
 }
 
 /*
+ * Fx29 - LD F, Vx
  * Set I = location of sprite for digit Vx.
  * The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx.
  */
@@ -424,6 +455,7 @@ void ld_F_Vx(Chip8 *chip8) {
 }
 
 /*
+ * Fx33 - LD B, Vx
  * Store BCD representation of Vx in memory locations I, I+1, and I+2.
  * The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I,
  * the tens digit at location I+1, and the ones digit at location I+2.
@@ -437,6 +469,7 @@ void ld_bcd_Vx(Chip8 *chip8) {
 }
 
 /*
+ * Fx55 - LD [I], Vx
  * Store registers V0 through Vx in memory starting at location I.
  * The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
  */
@@ -449,6 +482,7 @@ void ld_regs_Vx(Chip8 *chip8) {
 }
 
 /*
+ * Fx65 - LD Vx, [I]
  * Read registers V0 through Vx from memory starting at location I.
  * The interpreter reads values from memory starting at location I into registers V0 through Vx.
  */
